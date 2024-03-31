f92fdf4569e5e6f09188ddc9bf79b87eea268ea7
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2024-03-31 22:08:58 +0530

    Add files via upload

---
+++ GetCookyServlet.java
@@ -0,0 +1,52 @@
+package com.cookiesdemo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GetCookyServlet")
+public class GetCookyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+		Cookie[] arr = request.getCookies();
+		String uname = "", site = "";
+		
+		for(Cookie c : arr) {
+			if(c.getName().equals("namecooky"))
+				uname = c.getValue();
+			if(c.getName().equals("sitecooky"))
+				site = c.getValue();
+		}
+		
+		pw.println("<!DOCTYPE html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+		pw.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">");
+		pw.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>");
+		pw.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\"></script>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<h1 style=\"color:green;"
+		+ "text-align:center\"> Your name : "+uname+
+		"</h1>");
+		pw.println("<h1 style=\"color:green;"
+				+ "text-align:center\"> Your site : "
+				+site+"</h1>");
+		pw.println("</body>");
+		pw.println("</html>");
+	}
+
+}
